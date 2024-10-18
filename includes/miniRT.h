d0982ffadd68a8673de9954e8f3dc62f6c3ea544
Henrizz <henriettezimmermann@hotmail.de>
2024-10-18 18:32:02 +0200

    test push Makefile and mini main

---
+++ includes/miniRT.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hzimmerm <hzimmerm@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 16:39:54 by hzimmerm          #+#    #+#             */
+/*   Updated: 2024/10/16 17:20:28 by hzimmerm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include "../libft/libft.h"
+# include "vector_setup.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/* utils */
+int	error_return(char *mssg);
+
+#endif 
---
+++ includes/vector_setup.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_setup.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hzimmerm <hzimmerm@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 17:19:57 by hzimmerm          #+#    #+#             */
+/*   Updated: 2024/10/16 17:46:29 by hzimmerm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+typedef struct vec
+{
+	double e[3];
+}	t_vec;
+
+typedef t_vec point;
+typedef t_vec color;
+
+typedef struct ray
+{
+	point	orig;
+	t_vec	dir;
+}	t_ray;
+
+typedef struct sphere
+{
+	point	center;
+	double	radius;
+}	t_sphere;
+
