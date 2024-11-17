4068d7b97fdae180cd6c5f6c8e59f651cd33641b
Henriette Zimmermann <hzimmerm@c3a8c2.42berlin.de>
2024-11-17 17:28:04 +0100

    start commit, for clean working tree

--- srcs/color_calculation/hit_objects.c
+++ srcs/color_calculation/hit_objects.c
@@ -6,7 +6,7 @@
 /*   By: hzimmerm <hzimmerm@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 14:58:46 by hzimmerm          #+#    #+#             */
-/*   Updated: 2024/11/17 16:24:33 by hzimmerm         ###   ########.fr       */
+/*   Updated: 2024/11/17 17:27:58 by hzimmerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ t_color	get_ray_color(t_ray ray, t_scene *scene)
 	find_closest(ray, scene, &obj);
 	if (obj.distance != INFINITY)
 	{
-		obj.hit_point = get_hit_point(ray, scene, obj.distance);
+		obj.hit_point = get_hit_point(ray, obj.distance);
 		color = calculate_obj_color(ray, scene, &obj);
 	}
 	else
@@ -57,7 +57,25 @@ void	find_closest(t_ray ray, t_scene *scene, t_closest *obj)
 	//continue with other objects
 }
 
+/* takes as input a ray and a sphere, returns -1 if no intersection has been found or a double t which is th position of intersection on the ray */
+double find_t_sphere(t_ray ray, t_sphere *sphere)
+{
+	t_vec orig_to_center_sph;
+	double dot_a;
+	double dot_b;
+	double sqrt_discriminant;
 
+	orig_to_center_sph = vec1_minus_vec2(sphere->center, ray.orig);
+	/*double a = vec3_dot(&r->dir, &r->dir); // a: This is the dot product of the ray direction with itself a=B⋅B 
+	double b = -2.0 * vec3_dot(&r->dir, &oc); // b: This is the dot product of the ray direction and the vector from the ray origin to the sphere center (oc), scaled by −2.0: b=−2(B⋅oc)
+	double c = vec3_dot(&oc, &oc) - radius * radius; // calculates the squared distance from the ray origin to the sphere, minus the square of the radius. This term checks if the ray starts inside or outside the sphere.
+	double discriminant = b * b - 4 * a * c; // Calculate the discriminant
+	if (discriminant < 0)
+    		return (-1.0); //no intersection
+	
+		return ((-b - sqrt(discriminant)) / (2.0 * a));*/
+	
+}
 t_color	calculate_obj_color(t_ray ray, t_scene *scene, t_closest *obj)
 {
 	
@@ -68,3 +86,6 @@ t_color	calculate_background_color(t_ray ray, t_scene *scene)
 {
 	
 }
+
+
+
