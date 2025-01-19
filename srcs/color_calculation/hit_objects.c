b85bda4a359e1fb71231bf762cad30e5b2686d12
hzimmerm <hzimmerm@student.42berlin.de>
2025-01-19 14:03:17 +0100

    merge adjustment

--- srcs/color_calculation/hit_objects.c
+++ srcs/color_calculation/hit_objects.c
@@ -6,7 +6,7 @@
 /*   By: hzimmerm <hzimmerm@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 14:58:46 by hzimmerm          #+#    #+#             */
-/*   Updated: 2025/01/10 18:28:26 by hzimmerm         ###   ########.fr       */
+/*   Updated: 2025/01/19 14:02:40 by hzimmerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,9 @@ t_color	get_ray_color(t_ray *ray, t_scene *scene)
 	} 
 	else if (obj.distance != INFINITY) // if distance has been updated, meaning object has been hit
 	{
-		// debug("object has been hit\n");
-		if (obj.type == SPHERE)
-			color = calculate_obj_color(scene, &obj);
-		else if (obj.type == PLANE)
-			color = calculate_obj_color(scene, &obj);
-		else if (obj.type == CYL)
-			color = obj.col;
-		else
-			color = create_triple(0,0,0);
 		obj.hit_point = get_hit_point(ray, obj.distance);
 		// debug("Object distance: %f , Object color: (%f, %f, %f)", obj.distance, color.e[0], color.e[1], color.e[2]);
-		// color = calculate_obj_color(scene, &obj);
+		color = calculate_obj_color(scene, &obj);
 	}
 	else
 		color = create_triple(0, 0, 0);
