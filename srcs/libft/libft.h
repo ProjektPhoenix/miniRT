7efc19b65b918cbec218ae8a5e93da82d64ca2fa
projektphoenix <rpriess@student.42berlin.de>
2025-01-23 22:47:52 +0100

    (ref) libft.h: norm
    still misaligned function declaration

--- srcs/libft/libft.h
+++ srcs/libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Henriette <Henriette@student.42.fr>        +#+  +:+       +#+        */
+/*   By: rpriess <rpriess@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 13:25:34 by hzimmerm          #+#    #+#             */
-/*   Updated: 2024/12/02 19:34:59 by Henriette        ###   ########.fr       */
+/*   Updated: 2025/01/23 22:47:08 by rpriess          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ size_t	ft_strlcpy(char *dst, char *src, size_t	size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	*ft_calloc(size_t count, size_t size);
 int		ft_atoi(const char *str);
-double		ft_atod(const char *str);
+double	ft_atod(const char *str);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
